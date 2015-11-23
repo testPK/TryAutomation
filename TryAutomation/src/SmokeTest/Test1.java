95c2f3d697f0cdfd4bf25142ea9218e4dba42fdf
testPK <fortestingallsites@gmail.com>
2015-11-23 08:59:37 -0800

    Added few files

---
+++ TryAutomation/src/SmokeTest/Test1.java
@@ -0,0 +1,12 @@
+package SmokeTest;
+import org.testng.annotations.Test;
+
+
+public class Test1 {
+
+	@Test
+	public void test1()
+	{
+		System.out.println("This is test1:");
+	}
+}
---
+++ TryAutomation/src/SmokeTest/Test2.java
@@ -0,0 +1,12 @@
+package SmokeTest;
+import org.testng.annotations.Test;
+
+
+public class Test2 {
+
+	@Test
+	public void test2()
+	{
+		System.out.println("This is test2:");
+	}
+}
